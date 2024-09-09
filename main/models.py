5ee2a16fa8b24c918076970ee99182d987c5e99a
mawlaraditya <mawlaraditya@gmail.com>
2024-09-09 15:55:34 +0700

    progres 3

--- main/models.py
+++ main/models.py
@@ -12,3 +12,11 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+    
+
+
+class Hat(models.Model):
+    name = models.CharField(max_length = 225)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.CharField(max_length=22083)
