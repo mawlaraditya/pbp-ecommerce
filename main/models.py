263fe79c41da01e40b82c21f9bea7c16926d82bd
mawlaraditya <mawlaraditya@gmail.com>
2024-09-10 19:16:56 +0700

    progres 6

--- main/models.py
+++ main/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+# class MoodEntry(models.Model):
+#     mood = models.CharField(max_length=255)
+#     time = models.DateField(auto_now_add=True)
+#     feelings = models.TextField()
+#     mood_intensity = models.IntegerField()
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+#     @property
+#     def is_mood_strong(self):
+#         return self.mood_intensity > 5
     
 
 
 class Hat(models.Model):
     name = models.CharField(max_length = 225)
     desc = models.TextField()
-    price = models.FloatField()
+    price = models.IntegerField()
     stock = models.IntegerField()
     image = models.CharField(max_length=22083)
--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Hat
 
-
 def home (request) :
     hat = Hat.objects.all()
     return render(request, 'home.html', {'hat' : hat})
